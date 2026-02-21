8f4281a2ee30b7a0183aaa811fb14e945558bff4
MatiasdeBuren <64704995+MatiasdeBuren@users.noreply.github.com>
2026-02-21 19:23:51 -0300

    feat(expenses): add endpoint to retrieve expense types with subtypes

--- src/routes/expenseRoutes.ts
+++ src/routes/expenseRoutes.ts
@@ -3,11 +3,13 @@ import { requireAuth } from "../auth_middleware";
 import {
   getUserExpenses,
   getUserExpensesSummary,
-  getUserExpense
+  getUserExpense,
+  getExpenseTypes
 } from "../controllers/expensesController";
 
 const router = Router();
 
+router.get("/types",   requireAuth, getExpenseTypes);
 router.get("/summary", requireAuth, getUserExpensesSummary);
 router.get("/",        requireAuth, getUserExpenses);
 router.get("/:id",     requireAuth, getUserExpense);
